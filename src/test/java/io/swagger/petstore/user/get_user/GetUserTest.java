6617d62a260e301e99b96bc6c88b5c89d68809f0
Unknown <artem.ustimchik@gmail.com>
2018-08-01 13:43:49 -0300

    add new test

---
+++ src/test/java/io/swagger/petstore/user/get_user/GetUserTest.java
@@ -0,0 +1,23 @@
+package io.swagger.petstore.user.get_user;
+
+import io.swagger.petstore.controllers.user.UserController;
+import io.swagger.petstore.models.user.UserModel;
+import io.swagger.petstore.user.UserDataGenerator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GetUserTest extends UserDataGenerator {
+
+    @Test
+    public void getUserTest() {
+
+        UserModel user = userModelGenerator();
+
+        UserController userController = new UserController(user);
+        userController.addUser();
+
+        UserModel responseUser = userController.getUser();
+
+        Assert.assertEquals(responseUser, user);
+    }
+}
